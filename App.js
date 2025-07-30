31a60313fea3ba53a90a78b04b658aacf7fdaad0
Priyansh   Dhiman <Priyansh.dhiman25@gmail.com>
2025-07-30 18:38:19 -0500

    Added a couple of text tags and styled the as well.

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image} from 'react-native';
 
 // View -> UIView 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello React Native World </Text>
-      <StatusBar style="auto" />
+
+    <Image source={Logo} />
+
+      <Text style ={styles.title}> 
+        The Number 1 
+      </Text>
+
+      <Text style = {{marginTop: 10, marginBottom: 30}}>
+        The Game APP
+      </Text>
+
+      <View style ={styles.APP}>
+        <Text>Hello, This is an APP.</Text>
+      </View>
+
+      <StatusBar style="auto"/>
+      
     </View>
   );
 }
@@ -17,6 +32,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title:{
+    fontWeight: 'bold',
+    fontSize: 18
+  },
+  APP:{
+    backgroundColor: '#eee',
+    padding: 20,
+    borderRadius: 10,
+    boxShadow: '4px 4px rgba(0,0,0,0.1)',
   },
 });
 
